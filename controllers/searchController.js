ed8754c15ddc1b9cd456f76fa25ebaabe8a1e8eb
Pramod Shah <prashah7@publicisgroupe.net>
2022-09-15 11:49:10 +0530

    content tyep

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -17,7 +17,6 @@ exports.getAllNearByPlaces = async (req, res) => {
         method: 'POST',
         url: 'https://pincode.p.rapidapi.com/',
         headers: {
-          'content-type': 'application/json',
           'Content-Type': 'application/json',
           'X-RapidAPI-Key': '40c206a2d2msh2fd5570284ae8cbp1edc77jsncca7677fdbee',
           'X-RapidAPI-Host': 'pincode.p.rapidapi.com'
